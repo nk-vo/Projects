792281dbc71e319da755e47f63e4992e0ea5e38b
nick <vo.nick2686@gmail.com>
2021-10-05 16:19:48 -0700

    Some minor changes to filename and code details

--- Python/pdf2word/pdf2docx.py
+++ Python/pdf2word/pdf2docx.py
@@ -4,10 +4,9 @@ from typing import Tuple
 
 
 def convert_pdf2docx(input_file, output_file, pages: Tuple = None):
-    """Converts pdf to docx"""
     if pages:
         pages = [int(i) for i in list(pages) if i.isnumeric()]
-    result = parse(pdf_file=input_file, docx_with_path=output_file, pages=pages)
+    result = parse(pdf_file = input_file, docx_with_path = output_file, pages = pages)
     summary = { "File: ": input_file, "Pages: ": str(pages), "Output File: ": output_file}
     # Printing Summary
     print("Summary")
