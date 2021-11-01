92cbbac4feb026b75a6aebc24dcc67bd3b821dd2
nick <vo.nick2686@gmail.com>
2021-11-01 00:04:33 -0700

    some changes to find primeFactors

--- Python/beginner/find_prime_factorial.py
+++ Python/beginner/find_prime_factorial.py
@@ -1,23 +1,31 @@
-factors = lambda n: [x for x in range(1,n+1) if not n%x]
-is_prime = lambda n: len(factors(n))==2
-primefactors = lambda n: list(filter(is_prime ,factors(n)))
+import math
 
-def primeFactorize(n):
-	n = int(n)
-	f = primefactors(n)
-	if is_prime(n):
-		return str(n)
-	else:
-		return str(f[0])+ "*"+primeFactorize(n/f[0])
-
-if __name__=='__main__':
-	print ("Welcome to the Prime Factorizer.. Enter the numbers in the prompt or enter 'quit' to exit")
-	num=0;
-	
-	while True:
-		if num:
-			print(primeFactorize(num))
-		print(">>>",end='')
-		num=input()
-		if num=="quit":
-			break
+# A function to print all prime factors of
+# a given number n
+def primeFactors(n):
+     
+    # Print the number of two's that divide n
+    while n % 2 == 0:
+        print(2),
+        n = n / 2
+         
+    # n must be odd at this point
+    # so a skip of 2 ( i = i + 2) can be used
+    for i in range(3,int(math.sqrt(n))+1,2):
+         
+        # while i divides n , print i and divide n
+        while n % i== 0:
+            print(i)
+            n = n / i
+             
+    # Condition if n is a prime
+    # number greater than 2
+    if n > 2:
+        print(n)
+         
+# Driver Program to test above function
+while True:
+    n = int(input("Enter desired Nth Fibbonaci number: "))
+    if n >= 0 and n <= 1000:
+        break
+primeFactors(n)
